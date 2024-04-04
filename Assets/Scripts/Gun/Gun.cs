c653ce27d23618f7a1a1744c9d207a71fc4f59b5
Alkomenko <danil.kublitsky@gmail.com>
2024-04-04 17:35:14 +0300

    Joystick attack

--- Assets/Scripts/Gun/Gun.cs
+++ Assets/Scripts/Gun/Gun.cs
@@ -6,28 +6,60 @@ public class Gun : MonoBehaviour
 {
     public float offset;
     public GameObject bullet;
+    public Joystick joystick;
     public Transform shotPoint;
-
     private float timeBtwShots;
     public float startTimeBtwShots;
-    void Update()
+    private Player player;
+    private Vector3 difference;
+    private float rotZ;
+    
+    private void Start()
     {
-        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        if (player.controlType == Player.ControlType.PC)
+        {
+            joystick.gameObject.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        if (player.controlType == Player.ControlType.PC)
+        { 
+            difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;  
+        }
+        else if (player.controlType == Player.ControlType.Android && Mathf.Abs(joystick.Horizontal) > 0.3f || Mathf.Abs(joystick.Vertical) > 0.3f)
+        { 
+            rotZ = Mathf.Atan2(joystick.Vertical, joystick.Horizontal) * Mathf.Rad2Deg; 
+        }
         transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
 
         if (timeBtwShots <= 0)
         {
-            if (Input.GetMouseButton(0))
+            if (Input.GetMouseButton(0) && player.controlType == Player.ControlType.PC)
             {
-                Instantiate(bullet, shotPoint.position, transform.rotation);
-                timeBtwShots = startTimeBtwShots;
-            }  
+                Shoot();
+            } 
+            else if (player.controlType == Player.ControlType.Android)
+            {
+                if (joystick.Horizontal != 0 || joystick.Vertical != 0)
+                {
+                    Shoot();
+                }
+            }
         }
         else
         {
             timeBtwShots -= Time.deltaTime;
         }
+    }
+
+    public void Shoot()
+    {
+        Instantiate(bullet, shotPoint.position, transform.rotation);
+        timeBtwShots = startTimeBtwShots;
     }
     
 }
