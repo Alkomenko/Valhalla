b9655a144f808b0d7cbeea80ada30c9459c2415e
Alkomenko <danil.kublitsky@gmail.com>
2024-04-04 16:12:44 +0300

    LobbyScene

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -8,7 +8,7 @@ using Random = System.Random;
 
 public class EnemySpawn : MonoBehaviour
 {
-    public GameObject plusPrefab;
+    public GameObject starPrefab;
     public GameObject enemyPrefab;
     public float timeRateSpawn;
     private float timeRate;
@@ -18,7 +18,7 @@ public class EnemySpawn : MonoBehaviour
         if (timeRate <= 0)
         {
             StartCoroutine(Spawn());
-            timeRate = timeRateSpawn;
+            timeRate = 5;
         }
         else
         {
@@ -31,7 +31,9 @@ public class EnemySpawn : MonoBehaviour
         Vector2 topLeft = Camera.main.ViewportToWorldPoint(new Vector3(0, 1));
         Vector2 downRight = Camera.main.ViewportToWorldPoint(new Vector3(1, 0));
         Vector2 RandomPos = new Vector2 (UnityEngine.Random.Range(topLeft.x, downRight.x), UnityEngine.Random.Range(downRight.y, topLeft.y));
-        GameObject plus = Instantiate(plusPrefab, RandomPos, quaternion.identity);
-        yield return null;
+        GameObject star = Instantiate(starPrefab, RandomPos, quaternion.identity);
+        yield return new WaitForSeconds(0.3f);
+        Instantiate(enemyPrefab, star.transform.position, Quaternion.identity);
+        Destroy(star);
     }
 }
