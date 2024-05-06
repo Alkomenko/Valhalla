5d2f245fcec6e702d51eeb1a44ee915b2bcf6a1d
sageoff <bbekzlll2@gmail.com>
2024-05-06 20:58:14 +0300

    sound add on buttons
    Добавлен звук ко всем кнопкам. Звук от кнопок будет выключаться при выключении VOLUME, при отключении MUSIC, отключается только музыка.

--- Assets/Scripts/MusicAndSounds/Sounds.cs
+++ Assets/Scripts/MusicAndSounds/Sounds.cs
@@ -9,67 +9,8 @@ public class Sounds : MonoBehaviour
     public static bool CheckVolumeButton = true;
     public static bool CheckMusicButton = true;
     private AudioSource audioSrc => GetComponent<AudioSource>();
-
-    public void PlaySound(AudioClip clip, 
-                            bool destroyed = false, 
-                            float p1 = 0.85f, 
-                            float p2 = 1.2f)
-    {
-        // audioSrc.pitch = Random.Range(p1, p2);
-        if (CheckVolumeButton == true)
-        {
-            audioSrc.PlayOneShot(clip, 1f);
-        }
-        if (CheckVolumeButton == false)
-        {
-            audioSrc.PlayOneShot(clip, 0f);
-        }
-        
-        // audioSrc.PlayOneShot(clip, volume);
-    }
-    
-    public void ResetVolumeButton()
+    public void OnClick()
     {
         audioSrc.PlayOneShot(sounds[0], 1f);
-        if (CheckVolumeButton)
-        {
-            CheckVolumeButton = false;
-        }
-        else
-        {
-            CheckVolumeButton = true;
-        }
-    }
-
-    public void ResetMusicButton()
-    {
-        PlayMusic(sounds[0]);
-        if (CheckMusicButton)
-        {
-            CheckMusicButton = false;
-        }
-        else
-        {
-            CheckMusicButton = true;
-        }
-    }
-    
-    public void PlayMusic(AudioClip clip, 
-        bool destroyed = false, 
-        float p1 = 0.85f, 
-        float p2 = 1.2f)
-    {
-        if (CheckMusicButton == true)
-        {
-            audioSrc.PlayOneShot(clip, 1f);
-            AudioListener.volume = 1f;
-        }
-        if (CheckMusicButton == false)
-        {
-            audioSrc.Stop();
-            AudioListener.volume = 1f;
-            // audioSrc.PlayOneShot(clip, 0f);
-            //audioSrc.IsDestroyed();
-        }
     }
 }
